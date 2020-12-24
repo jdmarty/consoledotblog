8c3a060437d567bfd98d5661e6b0e96ddbc5f1d4
JD Marty <joshua.dmarty@gmail.com>
2020-12-23 17:31:47 -0800

    added comments and email checking to userRoutes for account handling

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,14 +1,26 @@
 const router = require('express').Router();
 const { User } = require('../../models');
 
-router.post('/', async (req, res) => {
+//create new user
+router.post('/create', async (req, res) => {
   try {
+    //look for a user with this email
+    const existingUser = await User.findOne({
+      where: { email: req.body.email },
+    });
+    //if there is a user with this email, send an error message and return
+    if (userData) {
+      res
+        .status(400)
+        .json({ message: 'Account with this username already exists, please try again' });
+      return;
+    }
+    //otherwise, create a new user
     const userData = await User.create(req.body);
-
+    //set loggedIn to true and save the user id in session
     req.session.save(() => {
       req.session.user_id = userData.id;
       req.session.logged_in = true;
-
       res.status(200).json(userData);
     });
   } catch (err) {
@@ -16,39 +28,42 @@ router.post('/', async (req, res) => {
   }
 });
 
+//log in existing user
 router.post('/login', async (req, res) => {
   try {
+    //find a user with that email
     const userData = await User.findOne({ where: { email: req.body.email } });
-
+    //if no user is found with that email, send an error message and return
     if (!userData) {
       res
         .status(400)
         .json({ message: 'Incorrect email or password, please try again' });
       return;
     }
-
+    //check password input against stored password
     const validPassword = await userData.checkPassword(req.body.password);
-
+    //if the passwords do not match, send an error message and return
     if (!validPassword) {
       res
         .status(400)
         .json({ message: 'Incorrect email or password, please try again' });
       return;
     }
-
+    //set loggedIn to true and save the user id in session
     req.session.save(() => {
       req.session.user_id = userData.id;
       req.session.logged_in = true;
-      
+
       res.json({ user: userData, message: 'You are now logged in!' });
     });
-
   } catch (err) {
     res.status(400).json(err);
   }
 });
 
+//logout a user
 router.post('/logout', (req, res) => {
+  //destroy the existing session
   if (req.session.logged_in) {
     req.session.destroy(() => {
       res.status(204).end();
