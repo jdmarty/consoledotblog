ded262677b05747b70a6e248b519c4d05732ca44
JD Marty <joshua.dmarty@gmail.com>
2020-12-23 19:54:22 -0800

    added basic navbar to main.handlebars

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -4,23 +4,13 @@ const withAuth = require('../utils/auth');
 
 router.get('/', async (req, res) => {
   try {
-    // Get all projects and JOIN with user data
-    const projectData = await Project.findAll({
-      include: [
-        {
-          model: User,
-          attributes: ['name'],
-        },
-      ],
-    });
-
-    // Serialize data so the template can read it
-    const projects = projectData.map((project) => project.get({ plain: true }));
-
     // Pass serialized data and session flag into template
+
+
+
     res.render('homepage', { 
-      projects, 
-      logged_in: req.session.logged_in 
+      logged_in: req.session.logged_in,
+      features: true
     });
   } catch (err) {
     res.status(500).json(err);
@@ -39,10 +29,12 @@ router.get('/project/:id', async (req, res) => {
     });
 
     const project = projectData.get({ plain: true });
+    // Serialize data so the template can read it
+    const projects = projectData.map((project) => project.get({ plain: true }));
 
     res.render('project', {
       ...project,
-      logged_in: req.session.logged_in
+      logged_in: req.session.logged_in,
     });
   } catch (err) {
     res.status(500).json(err);
@@ -72,7 +64,7 @@ router.get('/profile', withAuth, async (req, res) => {
 router.get('/login', (req, res) => {
   // If the user is already logged in, redirect the request to another route
   if (req.session.logged_in) {
-    res.redirect('/profile');
+    res.redirect('/');
     return;
   }
 
